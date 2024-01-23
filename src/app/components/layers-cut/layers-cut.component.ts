436e4a3a21d31ebd035684387ea08e6aedbd4067
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-01-23 01:14:18 +0100

    layers cut

---
+++ src/app/components/layers-cut/layers-cut.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layers-cut',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layers-cut.component.html',
+  styleUrl: './layers-cut.component.scss'
+})
+export class LayersCutComponent {
+
+}
--- src/app/components/layers/layers.component.ts
+++ src/app/components/layers/layers.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayerBoxComponent } from '../layer-box/layer-box.component';
 import { CanvasNavigationComponent } from '../canvas-navigation/canvas-navigation.component';
+import { LayersCutComponent } from '../layers-cut/layers-cut.component';
 
 @Component({
   selector: 'app-layers',
   standalone: true,
-  imports: [CommonModule, LayerBoxComponent, CanvasNavigationComponent],
+  imports: [CommonModule, LayerBoxComponent, CanvasNavigationComponent, LayersCutComponent],
   templateUrl: './layers.component.html',
   styleUrl: './layers.component.scss'
 })
