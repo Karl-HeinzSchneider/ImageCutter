f1c7405b024dc3a17a4d5f78504f524538bced43
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-01-23 09:12:54 +0100

    nonlinear slider

--- src/app/components/canvas-navigation/canvas-navigation.component.ts
+++ src/app/components/canvas-navigation/canvas-navigation.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRepository, ImageProps } from '../../state/cutter.store';
 import { Observable, Subject, debounceTime, takeUntil } from 'rxjs';
 
+// fn: https://www.wolframalpha.com/input?i=exp+fit+%7B%7B0%2C0.1%7D%2C%7B30%2C1%7D%2C%7B100%2C50%7D%7D
+// inverse: https://www.wolframalpha.com/input?i=Umkehrfunktion+f%28x%29%3D0.18129+e%5E%280.0561968+x%29
+@Pipe({ name: 'inverseExp', standalone: true })
+export class inverseExpPipe implements PipeTransform {
+
+  transform(val: number) {
+    return 17.7946 * Math.log(5.516024049864857 * val)
+  }
+}
+
 @Component({
   selector: 'app-canvas-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, inverseExpPipe],
   templateUrl: './canvas-navigation.component.html',
   styleUrl: './canvas-navigation.component.scss'
 })
@@ -39,10 +49,14 @@ export class CanvasNavigationComponent implements OnDestroy, OnInit {
     //console.log(e)
     const tar = e.target as HTMLInputElement
 
-    const value = Number(tar.value)
+    const value = this.scaleValue(Number(tar.value))
     //console.log(tar.value)
     //this.store.updateZoom(id, value)
     this.updateSubject.next([id, value])
+  }
+
+  scaleValue(val: number): number {
+    return 0.18129 * Math.exp(0.0561968 * val)
   }
 
 }
