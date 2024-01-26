3893cef7bdf7375091a36ba92270ad129d64a329
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-01-26 07:42:25 +0100

    init canvas preview

--- src/app/components/canvas-navigation/canvas-navigation.component.ts
+++ src/app/components/canvas-navigation/canvas-navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnDestroy, OnInit, Pipe, PipeTransform, ViewChil
 import { CommonModule } from '@angular/common';
 import { AppRepository, ImageProps } from '../../state/cutter.store';
 import { Observable, Subject, debounceTime, takeUntil } from 'rxjs';
+import { CanvasPreviewComponent } from '../canvas-preview/canvas-preview.component';
 
 // fn: https://www.wolframalpha.com/input?i=exp+fit+%7B%7B0%2C0.1%7D%2C%7B30%2C1%7D%2C%7B100%2C50%7D%7D
 // inverse: https://www.wolframalpha.com/input?i=Umkehrfunktion+f%28x%29%3D0.18129+e%5E%280.0561968+x%29
@@ -16,7 +17,7 @@ export class inverseExpPipe implements PipeTransform {
 @Component({
   selector: 'app-canvas-navigation',
   standalone: true,
-  imports: [CommonModule, inverseExpPipe],
+  imports: [CommonModule, inverseExpPipe, CanvasPreviewComponent],
   templateUrl: './canvas-navigation.component.html',
   styleUrl: './canvas-navigation.component.scss'
 })
---
+++ src/app/components/canvas-preview/canvas-preview.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-canvas-preview',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './canvas-preview.component.html',
+  styleUrl: './canvas-preview.component.scss'
+})
+export class CanvasPreviewComponent {
+
+}
