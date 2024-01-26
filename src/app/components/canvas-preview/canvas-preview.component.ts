cffaaeae283be58a554960fc545d14ca88cd2d77
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-01-26 08:44:41 +0100

    canvas preview

--- src/app/components/canvas-preview/canvas-preview.component.ts
+++ src/app/components/canvas-preview/canvas-preview.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Stage } from 'konva/lib/Stage';
+import { Layer } from 'konva/lib/Layer';
+import Konva from 'konva';
+import { Subject, takeUntil } from 'rxjs';
+import { AppRepository, ImageFile } from '../../state/cutter.store';
+import { getActiveEntity } from '@ngneat/elf-entities';
 
 @Component({
   selector: 'app-canvas-preview',
@@ -8,6 +14,105 @@ import { CommonModule } from '@angular/common';
   templateUrl: './canvas-preview.component.html',
   styleUrl: './canvas-preview.component.scss'
 })
-export class CanvasPreviewComponent {
+export class CanvasPreviewComponent implements AfterViewInit, OnChanges, OnDestroy {
+  private readonly destroy$ = new Subject<number>()
 
+  private stage!: Stage;
+  private layerBG!: Layer;
+
+  private id: string = '-42';
+  private imageFile: ImageFile | undefined;
+
+
+  constructor(private store: AppRepository) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.stage) {
+      this.initStage()
+      this.initSubs()
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(1)
+    this.destroy$.complete()
+  }
+
+  private initStage() {
+    this.stage = new Konva.Stage({
+      height: 117,
+      width: 208,
+      container: 'preview'
+    })
+
+    const layerBG = new Konva.Layer({
+      imageSmoothingEnabled: false
+    })
+    this.layerBG = layerBG
+    this.stage.add(this.layerBG)
+  }
+
+  private initSubs() {
+    // ImageFile
+    this.store.activeFile$.pipe(takeUntil(this.destroy$)).subscribe(f => {
+      console.log('file changed', f)
+      this.imageFile = f;
+
+      // #hack, need id for store update...
+      const q = this.store.store.query(getActiveEntity())
+      if (q) {
+        this.id = q.id
+      }
+
+      this.drawBG()
+
+    })
+  }
+
+  private drawBG() {
+    const file = this.imageFile
+    if (!file) {
+      return;
+    }
+    const stageRef = this.stage;
+
+    const url = file.dataURL
+
+    const h = file.height;
+    const w = file.width;
+
+    const hScale = stageRef.height() / h;
+    const wScale = stageRef.width() / w
+
+    const scale = Math.min(hScale, wScale)
+
+    const dx = (stageRef.width() - w * scale) / 2
+    const dy = (stageRef.height() - h * scale) / 2
+
+    //console.log(hScale, wScale, scale, dx, dy)
+
+    const layerRef = this.layerBG;
+    const bg = layerRef.findOne('#previewBG')
+    if (bg) {
+      layerRef.destroyChildren()
+    }
+
+    Konva.Image.fromURL(url, function (node) {
+      node.setAttrs({
+        x: dx,
+        y: dy,
+        scaleX: scale,
+        scaleY: scale,
+        id: 'previewBG'
+      })
+
+      layerRef.add(node)
+    })
+
+  }
 }
