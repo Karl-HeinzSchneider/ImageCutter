16e256c5f66699d9a5dfc5ce6b4a15b61f150e2d
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-01-31 07:36:01 +0100

    style layers export

--- src/app/components/layers-export/layers-export.component.ts
+++ src/app/components/layers-export/layers-export.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayerBoxComponent } from '../layer-box/layer-box.component';
+import { AppRepository, ImageProps } from '../../state/cutter.store';
+import { TooltipModule } from '../../modules/tooltip/tooltip.module';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layers-export',
   standalone: true,
-  imports: [CommonModule, LayerBoxComponent],
+  imports: [CommonModule, LayerBoxComponent, TooltipModule],
   templateUrl: './layers-export.component.html',
   styleUrl: './layers-export.component.scss'
 })
 export class LayersExportComponent {
 
+  public active$: Observable<ImageProps | undefined>;
+
+  constructor(private store: AppRepository) {
+    this.active$ = store.active$;
+  }
+
+  onClickSelected(active: ImageProps) {
+    console.log('onClickSelected')
+  }
+
+  onClickAll(active: ImageProps) {
+    console.log('onClickAll')
+  }
 }
