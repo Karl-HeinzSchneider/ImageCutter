f6b00a492afdedf5c5078c332e46c3acf74a0c85
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-02-05 06:04:38 +0100

    Improve layers (#7)
    * update layers-cuts
    
    * Update layers.component.html
    
    * fake canvas navigation

--- src/app/components/layers-cuts/layers-cuts.component.ts
+++ src/app/components/layers-cuts/layers-cuts.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LayerBoxComponent } from '../layer-box/layer-box.component';
 import { LayersCutComponent } from '../layers-cut/layers-cut.component';
 import { AppRepository, CanvasProps, ImageProps } from '../../state/cutter.store';
-import { Observable, distinctUntilChanged, from, map, switchMap } from 'rxjs';
+import { Observable, distinctUntilChanged, from, map, switchMap, tap } from 'rxjs';
 import { TooltipModule } from '../../modules/tooltip/tooltip.module';
 
 @Component({
@@ -15,88 +15,49 @@ import { TooltipModule } from '../../modules/tooltip/tooltip.module';
 })
 export class LayersCutsComponent {
 
-  @Input() active!: ImageProps;
+  active: ImageProps | undefined;
 
-  image$: Observable<HTMLImageElement>;
-
+  active$: Observable<ImageProps | undefined>;
   activeCanvas$: Observable<CanvasProps>;
 
   constructor(private store: AppRepository) {
+    this.active$ = store.active$.pipe(
+      tap(active => {
+        this.active = active;
+      })
+    );
+
     this.activeCanvas$ = store.activeCanvas$
-
-    const dataURL$ = store.active$.pipe(
-      map(active => {
-        return active?.file?.dataURL || ''
-      }),
-      distinctUntilChanged()
-    )
-
-    const test = from(new Promise(resolve => {
-      resolve('test')
-    }))
-
-    this.image$ = dataURL$.pipe(
-      switchMap(dataURL => {
-        const prom: Promise<HTMLImageElement> = new Promise((resolve, reject) => {
-          const img = new Image()
-
-          img.addEventListener('load', () => {
-            resolve(img)
-          })
-
-          img.src = dataURL
-        })
-
-        return prom
-      })
-    )
-
-    /* store.activeCanvas$.subscribe(c => {
-      console.log('ActiveCanvasChange', c)
-
-      const ctx = c.canvas.getContext('2d')
-
-      if (!ctx) {
-        console.log('no ctx')
-        return;
-      }
-      const data = ctx?.getImageData(0, 0, 100, 100)
-      console.log(data)
-
-      c.canvas.transferToImageBitmap()
-
-      const newCanv = document.createElement('canvas')
-      newCanv.getContext('2d')?.drawImage(c.canvas, 0, 0)
-
-      const blob = c.canvas.convertToBlob().then(b => {
-        const url = URL.createObjectURL(b)
-        console.log(url)
-      })
-    }) */
   }
 
   adderClicked() {
-    console.log('Adder', this.active)
-    this.store.addNewCut(this.active.id)
+    if (this.active) {
+      console.log('Adder', this.active)
+      this.store.addNewCut(this.active.id)
+    }
   }
 
   removerClicked() {
-    console.log('Remover', this.active)
+    if (this.active) {
+      console.log('Remover', this.active)
 
-    const selected = this.active.cuts?.find(x => x.selected)
+      const selected = this.active.cuts?.find(x => x.selected)
 
-    if (this.active.cuts && selected) {
-      this.store.removeCut(this.active.id, selected.id)
+      if (this.active.cuts && selected) {
+        this.store.removeCut(this.active.id, selected.id)
+      }
     }
   }
 
   duplicateClicked() {
-    console.log('Duplicate')
+    if (this.active) {
+      console.log('Duplicate')
 
-    const selected = this.active.cuts?.find(x => x.selected)
+      const selected = this.active.cuts?.find(x => x.selected)
 
-    if (this.active.cuts && selected) {
-      this.store.duplicateCut(this.active.id, selected.id)
+      if (this.active.cuts && selected) {
+        this.store.duplicateCut(this.active.id, selected.id)
+      }
     }
   }
 }
