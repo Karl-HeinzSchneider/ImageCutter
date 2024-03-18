976f6ad4e154e5c2eb9a458c1cad3023190f86f8
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-03-18 05:43:56 +0100

    toolbar ngswitch

--- src/app/components/toolbar/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar/toolbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarCutsComponent } from '../toolbar-cuts/toolbar-cuts.component';
-import { AppRepository, ImageProps } from '../../../state/cutter.store';
+import { AppRepository, ImageProps, tool } from '../../../state/cutter.store';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -14,8 +14,10 @@ import { Observable } from 'rxjs';
 export class ToolbarComponent {
 
   active$: Observable<ImageProps | undefined>
+  tool$: Observable<tool>;
 
   constructor(private store: AppRepository) {
     this.active$ = store.active$;
+    this.tool$ = store.tool$;
   }
 }
