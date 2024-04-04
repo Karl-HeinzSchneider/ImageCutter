f39e987bc59e2d2311a4ecbaf0add4d057aae8d1
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-04-04 05:00:05 +0200

    Improve tabs (#19)
    * fix size etc
    
    * overflow div and css
    
    * Update styles.scss
    
    * active always visible
    
    * context
    
    * overflow inactive, and sticky adder
    
    * fix adder tabview

--- src/app/components/tabbar/tabbar/tabbar.component.ts
+++ src/app/components/tabbar/tabbar/tabbar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from '../tabs/tabs.component';
 import { AppRepository, ImageProps } from '../../../state/cutter.store';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, distinctUntilChanged, map, shareReplay } from 'rxjs';
 
+export interface tabArrays {
+  tabs: ImageProps[],
+  overflow: ImageProps[]
+}
 @Component({
   selector: 'app-tabbar',
   standalone: true,
@@ -17,21 +21,109 @@ export class TabbarComponent {
   activeID$: Observable<string>;
   imagesOpen$: Observable<ImageProps[]>;
 
+
+  private maxTabs: BehaviorSubject<number> = new BehaviorSubject(4);
+  public maxTabs$ = this.maxTabs.pipe(distinctUntilChanged(), shareReplay());
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateSize()
+  }
+
+  tabs$: Observable<tabArrays>;
+
+  showContext: boolean = false;
+
+
   constructor(private store: AppRepository) {
     this.active$ = store.active$;
     this.imagesOpen$ = store.imagesOpen$;
 
     this.activeID$ = store.active$.pipe(map(prop => {
-      if (prop) {
-        return prop.id
-      }
-      else {
+      if (!prop) {
         return '-1'
       }
+      return prop.id
+    }))
+
+    this.updateSize();
+
+    this.tabs$ = combineLatest([this.imagesOpen$, this.maxTabs$, this.activeID$]).pipe(map(([images, maxTabs, activeID]) => {
+
+      let tabs: ImageProps[] = [];
+      let overflow: ImageProps[] = []
+
+      const tabsToShow = maxTabs - 1;
+
+      tabs = images.slice(0, tabsToShow);
+      overflow = images.slice(tabsToShow);
+
+      if (tabsToShow < 1) {
+        //console.log(tabs, overflow);
+        return { tabs: [], overflow: images }
+      }
+      else if (activeID === '-1') {
+        return { tabs: tabs, overflow: overflow }
+      }
+
+      //check if active images is in there
+      const active = tabs.find(x => x.id === activeID);
+
+      if (!active) {
+        const realActive = overflow.find(x => x.id === activeID)!;
+
+        const pop = tabs.pop()!;
+        tabs.push(realActive);
+
+        const overflowWithoutActive = overflow.filter(x => x.id != activeID);
+
+        overflow = [pop, ...overflowWithoutActive];
+      }
+
+      //console.log(images, maxTabs, activeID);
+      //console.log(tabs, overflow);
+
+      return { tabs: tabs, overflow: overflow }
     }))
   }
 
   onClick() {
     this.store.setActiveImage('-1')
+  }
+
+  updateSize() {
+    const innerW = window.innerWidth
+
+    const space = innerW - 48 - 240;
+
+    //console.log('tabbarSpace:', space);
+
+    // fixed min tabsize = 64px
+    const minSize = 128;
+
+    const tabs = Math.floor(space / minSize);
+
+    //console.log('tabs', tabs);
+
+    this.maxTabs.next(tabs);
+
+  }
+
+  onClickContext(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.showContext = !this.showContext;
+  }
+
+  onMouseLeave() {
+    this.showContext = false;
+  }
+
+  onSelect(e: Event, img: ImageProps) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('onSelect', img);
+
+    this.store.setActiveImage(img.id);
   }
 }
